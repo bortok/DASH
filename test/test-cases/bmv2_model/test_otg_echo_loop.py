83bc35a07c557f945018aeefe01a9b9fc61bb2de
Alex Bortok <dev@bortok.com>
2022-07-01 16:21:42 -0700

    changed logic to expect packets to echo back
    removed flow metrics test as flow metrics don't handle echo packets properly, see issue-ixia-c-flow-echo branch

--- test/test-cases/bmv2_model/test_otg_echo_loop.py
+++ test/test-cases/bmv2_model/test_otg_echo_loop.py
@@ -22,7 +22,7 @@ f1.tx_rx.port.tx_name, f1.tx_rx.port.rx_name = p1.name, p2.name
 f2.tx_rx.port.tx_name, f2.tx_rx.port.rx_name = p2.name, p1.name
 
 # how many packets to send
-pkt_count_max=200
+pkt_count_max=70
 # at what rate
 pps=10
 
@@ -52,7 +52,7 @@ udp1.src_port.increment.start = 32768
 udp1.src_port.increment.step = 53
 udp1.src_port.increment.count = 100
 
-udp2.src_port.increment.start = 32768
+udp2.src_port.increment.start = 32768 + 1024
 udp2.src_port.increment.step = 47
 udp2.src_port.increment.count = 100
 
@@ -61,7 +61,7 @@ udp1.dst_port.increment.start = 1024
 udp1.dst_port.increment.step = 67
 udp1.dst_port.increment.count = 100
 
-udp2.dst_port.increment.start = 1024
+udp2.dst_port.increment.start = 1024 + 1024
 udp2.dst_port.increment.step = 71
 udp2.dst_port.increment.count = 100
 
@@ -69,11 +69,11 @@ udp2.dst_port.increment.count = 100
 print("Pushing traffic configuration ...")
 api.set_config(cfg)
 
-def test_udp_bidirectional_ports():
+def test_echo_ports():
     """
     This test does following:
-    - Send packets from one port to another at a specified rate
-    - Validate that total packets sent from all ports were received by all the ports
+    - Send packets from a port at a specified rate
+    - Validate that all packets were echoed back to the same port
     """
     print("")
     print("Starting transmit...")
@@ -82,33 +82,24 @@ def test_udp_bidirectional_ports():
     api.set_transmit_state(ts)
 
     print("Checking metrics on all ports ...")
-    port_metrics_header = "Frames:\tExpected\tCurr:\tSum(Tx)\tSum(Rx)\t\t"
+    print("All packets should be echoed back to the same port")
+    port_metrics_header = ""
     for p in cfg.ports:
-        port_metrics_header += "Tx %s\tRx %s\t"%(p.name, p.name)
+        port_metrics_header += "| Port\tExpected\tCurrent\tTx\tRx\t"
     print(port_metrics_header)
     assert wait_for(lambda: port_metrics_ok(api, cfg), pkt_count_max / pps * 2), "Metrics validation failed!"
     
     print("Test passed !")
 
-def test_udp_bidirectional_flows():
-    """
-    This test does following:
-    - Send packets from one port to another at a specified rate
-    - Validate for each flow the number of received frames is no less than the number of transmitted frames
-    """
-    print("")
-    print("Starting transmit...")
-    ts = api.transmit_state()
-    ts.state = ts.START
-    api.set_transmit_state(ts)
-
-    print("Checking metrics on all flows ...")
-    print("Flow Name\tFrames:\tExpected\tCurrent\tTx\tRx\tRate:\tTx\tRx")
-    assert wait_for(lambda: flow_metrics_ok(api, cfg), pkt_count_max / pps * 2), "Metrics validation failed!"
-    
-    print("Test passed !")
-    
 def port_metrics_ok(api, cfg):
+    # expectations per port and in total
+    PortExpectedDict = {}
+    for f in cfg.flows:
+        if f.tx_rx.port.tx_name in PortExpectedDict:
+            current = PortExpectedDict[f.tx_rx.port.tx_name]['frames_expected']
+        else:
+            current = 0
+        PortExpectedDict.update({f.tx_rx.port.tx_name: {'frames_expected': current + f.duration.fixed_packets.packets}})
     # create a port metrics request and filter based on port names
     req = api.metrics_request()
     req.port.port_names = [p.name for p in cfg.ports]
@@ -117,37 +108,16 @@ def port_metrics_ok(api, cfg):
 
     # fetch port metrics
     res = api.get_metrics(req)
-    # calculate total frames sent and received across all configured ports
-    total_tx = sum([m.frames_tx for m in res.port_metrics])
-    total_rx = sum([m.frames_rx for m in res.port_metrics])
-    expected = sum([f.duration.fixed_packets.packets for f in cfg.flows])
-
-    PortMetricsDict = {}
+        
     for pm in res.port_metrics:
-        PortMetricsDict.update({pm.name: {'frames_tx': pm.frames_tx, 'frames_rx': pm.frames_rx}})
+        print("| %s\t\t%d\t\t%d\t%d" % (pm.name, PortExpectedDict[pm.name]['frames_expected'], pm.frames_tx, pm.frames_rx), end="\t")
 
-    print("\t%d\t\t\t%d\t%d\t\t%d\t%d\t%d\t%d" % (expected, total_tx, total_rx, PortMetricsDict['p1']['frames_tx'], PortMetricsDict['p1']['frames_rx'], PortMetricsDict['p2']['frames_tx'], PortMetricsDict['p2']['frames_rx']))
-
-    return expected == total_tx and total_rx >= expected
-
-def flow_metrics_ok(api, cfg):
-    # expectations per flow and in total
-    FlowExpectedDict = {}
-    for f in cfg.flows:
-        FlowExpectedDict.update({f.name: {'frames_expected': f.duration.fixed_packets.packets}})
-    # create a flow metrics request
-    req = api.metrics_request()
-    req.flow.flow_names = [f.name for f in cfg.flows]
-    
-    # fetch metrics
-    res = api.get_metrics(req)
+    print()
     completed = True # will check if there are any flows that are still running below
-    for fm in res.flow_metrics:
-        expected = FlowExpectedDict[fm.name]['frames_expected']
-        print("%s\t\t\t%d\t\t\t%d\t%d\t\t%d\t%d" % (fm.name, expected, fm.frames_tx, fm.frames_rx, fm.frames_tx_rate, fm.frames_rx_rate))
-        if expected != fm.frames_tx or fm.frames_rx < fm.frames_tx:
-            completed = False
-    
+    for pm in res.port_metrics:
+        pm.frames_rx < PortExpectedDict[pm.name]['frames_expected']
+        completed = False
+
     return completed
 
 def wait_for(func, timeout=10, interval=0.2):
